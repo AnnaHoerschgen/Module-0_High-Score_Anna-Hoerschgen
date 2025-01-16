8221398ed41d57b4e4a5fc52cb05ebbecaf92203
Anna Hoerschgen <ah0959247@otc.edu>
2025-01-16 12:01:02 -0600

    First Commit

---
+++ main.java
@@ -0,0 +1,16 @@
+// Imports
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+// main
+public class Main {
+    public static void main(String[] args) {
+        //
+    }
+}
+
+public class CSVReader {
+    String workingDir = System.getProperty(key:"user.dir");
+    String filePath = ""
+}
